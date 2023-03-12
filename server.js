7786f862ca356be478f9f71b8f8ad6037bff1ba8
nshaw973 <nshaw973@gmail.com>
2023-03-12 12:14:14 -0700

    Connected the html.index and notes.html as paths to each other. Also added a wildcard path that redirects the user to the main page for the moment.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//Homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+//Notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+//Wildcard
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+})
