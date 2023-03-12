bbfe4bfda5228012033e5ebb6a3da01b9ffd69e9
nshaw973 <nshaw973@gmail.com>
2023-03-12 15:10:04 -0700

    got uuid from npm to use for creating randomly generated ids for each note

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,33 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 
-const {
-    readFromFile,
-    readAndAppend,
-    writeToFile,
-  } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
-
+//Grabs the data in the db/db.json file and repopulates the page with it.
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
+//Allows user to search up specific note with id.
+notes.get('/:note_id', (req, res) => {
+
+    const noteId = req.params.note_id;
+
+        readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+
+            const result = json.filter((note) => note.note_id === noteId);
+
+            return result.length > 0
+
+            ? res.json(result)
+            : res.json('No note with that ID');
+
+    });
+});
+
+//This adds the title and text the user typed out, into the db/db.json file
 notes.post('/', (req, res) => {
   console.log(req.body);
 
@@ -20,6 +37,7 @@ notes.post('/', (req, res) => {
     const newNote = {
       title,
       text,
+      note_id: uuidv4(),
     };
 
     readAndAppend(newNote, './db/db.json');
